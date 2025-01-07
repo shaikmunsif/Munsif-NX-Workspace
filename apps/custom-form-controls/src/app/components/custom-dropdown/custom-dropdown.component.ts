bdfe0bef5863ac9fae72b9c52e12fdabf72feb86
shaikmunsif <shaikmunsif4@gmail.com>
2025-01-07 10:52:03 +0530

    created custom dropdown component

---
+++ apps/custom-form-controls/src/app/components/custom-dropdown/custom-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-custom-dropdown',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './custom-dropdown.component.html',
+  styleUrl: './custom-dropdown.component.scss',
+})
+export class CustomDropdownComponent {}
